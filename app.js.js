b1b37c331d9160a5f0eed75f23a28f033b13d8c8
blackBuzo2601 <elianpro58@gmail.com>
2023-10-07 21:54:33 -0700

    Validacion de cantidad de columnas listo

--- app.js.js
+++ app.js.js
@@ -19,17 +19,19 @@ fs.readFile('archivo.txt', 'utf8', (err, data) => {
     var renglones = data.split('\n'); //usamos el salto de linea para definir cada renglon
     const topColumnas = renglones[0]; //almacena los datos generales por el primer renglon
     const divisionColumnas = topColumnas.split(","); //separador de datos del topColumnas
-    const contadorColumnas = divisionColumnas.length-1; //contador de datos, puesto que es como un arreglo
-    var caracteresAlfanumericos="abcdefghijklmnopqrstuvwxyz";
+    const contadorColumnas = divisionColumnas.length-1; //contador de campos de informacion
+    var caracteresAlfanumericos="abcdefghijklmnopqrstuvwxyz0123456789";
     var letra;
     
 
-    console.log(renglones[0]);
-    for(let i=1;i<=renglones.length;i++){
-    var divisionColumnasEntrada=renglones[i].split(",");           
-    var contadorColumnasEntrada=divisionColumnasEntrada.length-1;
+    for(let i=1;i<=renglones.length;i++){ //ciclo for general que recorrera cada uno de los renglones
+        //empieza en el renglon 1 puesto que el 0 es el de el tipo de dato.
+
+    var divisionColumnasEntrada=renglones[i].split(",");            //separador de datos ingresados
+    var contadorColumnasEntrada=divisionColumnasEntrada.length-1;   //contador de datos ingresados
 
 
+    //VALIDAR SI LOS RENGLONES TIENEN LA MISMA CANTIDAD DE COLUMNAS QUE EL TopColumnas
         if(contadorColumnasEntrada==contadorColumnas){ 
         console.log("El renglon "+i+" si tiene la misma cantidad de columnas");
         }
@@ -43,6 +45,8 @@ fs.readFile('archivo.txt', 'utf8', (err, data) => {
         letra=renglones[i].charAt(j).toLowerCase();   
         if(caracteresAlfanumericos.includes(letra)){
                 console.log("Si es alfanumerico");
+        }else{
+            
         }
                
                
